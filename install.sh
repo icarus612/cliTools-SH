54c41d29e87513473d1fbec2c4cd10010f8e48e0
icarus64 <ellishogan95@gmail.com>
2023-12-02 15:51:41 -0500

    update

--- install.sh
+++ install.sh
@@ -8,7 +8,9 @@ function install_all() {
   local files_to_check=(.bashrc .bash_profile .zshrc .zprofile)
   local source_found=false
   local installed_items=()
-  
+  local bash_files=./bash_scripts/*.sh
+  local python_files=./python_scripts/*.py
+
   echo "Installing Daedalus: This is a linux/unix only script"
   if [[ -d "$dae_dir" ]]; then
     echo "Warning: This script will overwrite any existing daedalus installation"
@@ -16,40 +18,37 @@ function install_all() {
     rm -rf $dae_dir
   fi
   
-  local to_source=($dae_sh/*.sh)
   mkdir $dae_dir
   mkdir $dae_sh
   mkdir $dae_py
-  
-  for path in "${to_source[@]}"
-  do
-    echo "$path"
-  done
   touch $entry_file
   
-  for path in "${to_source[@]}"
-  do
-    echo "source $path" >> $entry_file 
-  done 
-  
-  
-  cp -r ./bash_scripts/*.sh $dae_sh
+  cp -r $bash_files $dae_sh
   chmod +x $dae_sh/*.sh
   echo "Added scripts to $dae_sh"
       
-  cp -r ./python_scripts/*.py $dae_py
+  cp -r $python_files $dae_py
   chmod +x $dae_py/*.py
   echo "Added scripts to $dae_py"
 
+  for file in "$dae_sh"/*.sh
+  do
+    if [[ "$file" != "$entry_file" ]]
+    then
+      echo "source $file" >> $entry_file 
+    fi
+  done
+  
   for rc_file in "${files_to_check[@]}"
   do
     local usr_rc=/home/$(logname)/$rc_file
-    
-    if [[ -f "$usr_rc" ]]; then
+
+    if [[ -f "$usr_rc" ]]
+    then
       source_found=true
       echo "Found $rc_file"
       local src_cmd="source $entry_file"
-      if grep -q $src_cmd $usr_rc
+      if grep -q "$src_cmd" $usr_rc
       then
         echo "Daedalus already installed in $rc_file"
       else 
@@ -67,7 +66,6 @@ function install_all() {
     echo "Please add the following line to your source file"
     echo "source $dae_sh"
   else 
-    echo "Source file found"
     echo "Installation complete"
     echo "Scripts installed:"
     installed_items+=($(grep -Prho "(?<=^function).+?(?=[\({])" $bash_files))
