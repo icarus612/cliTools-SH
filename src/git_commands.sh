c816529e2e179b24786ccdd0b0610af01ad1a798
Ellis Hogan <ellishogan@Elliss-MacBook-Air.local>
2024-01-03 11:22:54 -0500

    update

--- src/git_commands.sh
+++ src/git_commands.sh
@@ -24,6 +24,17 @@ function gsup() {
     gup "$message"
 }
 
+function gsupinit() {
+    local message=$1
+    if [[ -z "$message" ]]
+    then
+        echo "No commit message supplied, using default message"
+        message="update" 
+    fi
+    git submodule foreach --recursive "git add --all; git commit -m \"$message\"; git push origin main"
+    gup "$message"
+}
+
 function gmonoinit() {
     git submodule update --init --recursive
 }
