35fd40bedc2f3798f6998381545796af75e11910
icarus64 <ellishogan95@gmail.com>
2024-01-10 23:13:51 -0500

    update

--- src/git_commands.sh
+++ src/git_commands.sh
@@ -5,61 +5,67 @@ function gsfor() {
 }
 
 function gup() {
-    local message="update"
-    local branch=""
-    while getopts "bms" flag
-    do
-        case "${flag}" in
-            m) message=$OPTARG;;
-            b) branch=$OPTARG;;
-            s) gsfor "git add --all; git commit -m \"$message\"; git push $branch";;
-            \?) echo "Invalid option: -$OPTARG" >&2; exit 1;;
-        esac
-    done
-		
-    git add --all
-    git commit -m "$message"
-    git push $branch
+	local message="update"
+	local isSubmodule=false
+	local branch=""
+	while getopts "bms" flag
+	do
+		case "${flag}" in
+			m) message=$OPTARG;;
+			s) isSubmodule=true;;
+			b) branch=$OPTARG;;
+			\?) echo "Invalid option: -$OPTARG" >&2; exit 1;;
+		esac
+	done
+	
+	if [[ "$isSubmodule" = true ]]
+	then
+		gsfor "git add --all; git commit -m \"$message\"; git push $branch"
+	fi
+	
+	git add --all
+	git commit -m "$message"
+	git push $branch
 }
 
 function gsinit() {
-    git submodule update --init --recursive
+  git submodule update --init --recursive
 }
 
 function gclone() {
-    local name=$1
-    if [[ -n $2 ]]
-    then
-        name=$2 
-    fi
-    git clone git@github.com:icarus612/"$1".git $name
-    cd $name 
-    gsinit &
-    cd -
+	local name=$1
+	if [[ -n $2 ]]
+	then
+			name=$2 
+	fi
+	git clone git@github.com:icarus612/"$1".git $name
+	cd $name 
+	gsinit &
+	cd -
 }
 
 function gsadd() {
-    local name=$1
-    if [[ -n $2 ]]
-    then
-        name=$2 
-    fi
-    git submodule add git@github.com:icarus612/"$1".git $name
+	local name=$1
+	if [[ -n $2 ]]
+	then
+			name=$2 
+	fi
+	git submodule add git@github.com:icarus612/"$1".git $name
 }
 
 function gspull() {
-		local branch="main"
-		
-    while getopts "bif" flag
-		do
-			case "${flag}" in
-				b) branch=$OPTARG;;
-				i) git submodule update --init --recursive;;
-				f) git pull --recurse-submodules;;
-				\?) echo "Invalid option: -$OPTARG" >&2; exit 1;;
-			esac
-		done
-		
-    gsfor 'git pull origin $branch'
-		git pull
+	local branch="main"
+	
+	while getopts "bif" flag
+	do
+		case "${flag}" in
+			b) branch=$OPTARG;;
+			i) git submodule update --init --recursive;;
+			f) git pull --recurse-submodules;;
+			\?) echo "Invalid option: -$OPTARG" >&2; exit 1;;
+		esac
+	done
+	
+	gsfor 'git pull origin $branch'
+	git pull
 }
